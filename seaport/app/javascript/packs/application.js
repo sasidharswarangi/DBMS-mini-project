563590a5bef8334e5c1e307ea1418c1e7ef2ef0b
Abhishek Kumar <abhishekkumar8222@gmail.com>
2020-02-25 08:14:04 +0530

    Add startbootstrap theme

--- seaport/app/javascript/packs/application.js
+++ seaport/app/javascript/packs/application.js
@@ -7,6 +7,7 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("startbootstrap-sb-admin-2/js/sb-admin-2")
 
 import './src/application.scss'
 
